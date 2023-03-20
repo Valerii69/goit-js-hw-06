30f3d8fd160df03ffd2e6da2d6d82829df4f911c
Valerii Viun <2455521@gmail.com>
2023-03-20 15:38:00 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector("#ingredients");
+const itemsEl = [];
+// Варіант 1
+ingredients.forEach((ingredient) => {
+  const itemNewEl = document.createElement("li");
+  itemNewEl.textContent = ingredient;
+  itemNewEl.classList.add("item");
+  itemsEl.push(itemNewEl);
+});
+
+listEl.append(...itemsEl);
+
