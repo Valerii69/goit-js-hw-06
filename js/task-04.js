10f3bbc6d4e86b68e8adbe291e6f25872d297e75
Valerii Viun <2455521@gmail.com>
2023-03-25 12:29:54 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+let counterValue = 0;
+  const valueEl = document.querySelector('#value');
+  const decrementBtn = document.querySelector('[data-action="decrement"]');
+  const incrementBtn = document.querySelector('[data-action="increment"]');
+
+  decrementBtn.addEventListener('click', () => {
+    counterValue -=1;
+    
+    valueEl.textContent = counterValue;
+    console.log(counterValue);
+  });
+
+  incrementBtn.addEventListener('click', () => {
+    counterValue +=1;
+    
+    valueEl.textContent = counterValue;
+    console.log(counterValue);
+  });
