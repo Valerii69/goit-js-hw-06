fb17a3edbb41060acccc8b774a78b26558704788
Valerii Viun <2455521@gmail.com>
2023-03-26 17:45:01 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
 
+const form = document.querySelector(".login-form")
+
+// form.addEventListener("submit", onSubmit);
+
+const fullSubmit = evt => {
+    evt.preventDefault();
+   
+    const { email, password } = evt.currentTarget.elements;
+     if (email.value === "" || password.value === "") {
+        return alert("Всі поля повинні бути заповнені!");
+              }
+    
+    const userData = { email: email.value, password: password.value }
+  
+    console.log(userData);
+    evt.currentTarget.reset();
+    }
+          
+    form.addEventListener("submit", fullSubmit);
+
